b00977f4027208f8137ffff7455cd9e23fc18aab
Markow Kirill <122300549+pank4roker@users.noreply.github.com>
2024-01-29 09:49:19 +0200

    Add files via upload

---
+++ T16_3/DateClass.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+
+namespace T16_3
+{
+    /// <summary>
+    /// класс DateClass для работы с датой
+    /// </summary>
+    class DateClass
+    {
+        /// <summary>
+        ///поле DateTime data;
+        /// </summary>
+        private DateTime data;
+        /// <summary>
+        /// Поле, обозначающее день
+        /// </summary>
+        public int year;
+        /// <summary>
+        /// Поле, обозначающее день
+        /// </summary>
+        public int month;
+        /// <summary>
+        /// Поле, обозначающее день
+        /// </summary>
+        public int day;
+        /// <summary>
+        /// Constr. without param
+        /// </summary>
+        public DateClass() { }
+        /// <summary>
+        /// Constr. with param
+        /// </summary>
+        /// <param name="data">Задаём дату</param>
+        public DateClass(int year,int month,int day)
+        {
+            //this.year = year; this.month = month; this.day = day;
+            data = new DateTime(year, month, day);
+
+        }
+        /// <summary>
+        /// свойство, позволяющее установить или получить значение поле класса
+        /// </summary>
+        public DateTime Data { get => data; set => data = value; }
+        /// <summary>
+        /// вычислить дату предыдущего дня;
+        /// </summary>
+        /// <returns>Дата предыдущего дня</returns>
+        public DateTime Yesterday()
+        {
+            return data.AddDays(-1);
+        }
+        /// <summary>
+        ///  вычислить дату следующего дня
+        /// </summary>
+        /// <returns>Дата следующего дня</returns>
+        public DateTime Tomorrow()
+        {
+            return data.AddDays(1);
+        }
+        /// <summary>
+        /// определить сколько дней осталось до конца месяца.
+        /// </summary>
+        /// <returns>Количество дней, оставшихся до конца месяца</returns>
+        public int Residue()
+        {
+            return DateTime.DaysInMonth(data.Year, data.Month) - data.Day;
+        }
+        public DateTime Output()
+        {
+            return data;
+        }
+        public TimeSpan Interval(DateTime Data1,DateTime Data2)
+        {
+            TimeSpan interval = (Data1 - Data2);
+            return interval;
+        }
+
+    }
+}
---
+++ T16_3/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace T16_3
+{
+    internal class Program
+    {
+        static DateTime Input(string input)
+        {
+            WriteLine($"Введите {input} дату: ");
+            Write("День: ");
+            int day = Convert.ToInt32(ReadLine());
+            Write("Месяц: ");
+            int month = Convert.ToInt32(ReadLine());
+            Write("Год: ");
+            int year = Convert.ToInt32(ReadLine());
+            return new DateTime(year, month, day);
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                //Task 1
+
+                /*WriteLine($"Введите дату: ");
+                Write("День: ");
+                int day = Convert.ToInt32(ReadLine());
+                Write("Месяц: ");
+                int month = Convert.ToInt32(ReadLine());
+                Write("Год: ");
+                int year = Convert.ToInt32(ReadLine());
+
+                DateClass date1 = new DateClass(year, month, day);
+                Write($"{date1.Output().ToString("d")}\nПредыдущий день: {date1.Yesterday().ToString("d")}\nПоследующий день: {date1.Tomorrow().ToString("d")}\nКол-во дней до конца месяца: {date1.Residue()}");*/
+
+                //Task 2
+
+                DateTime date_1 = Input("первую");
+                DateTime date_2 = Input("вторую");
+                TimeSpan delta = date_1 - date_2;
+                WriteLine($"Первая дата: {date_1.ToShortDateString()}\nВторая дата: {date_2.ToShortDateString()}\nРазница: {delta.Days}");
+
+                //Task 3
+
+
+
+
+            }
+            catch (Exception ex)
+            {
+                WriteLine($"Исключение: {ex.Message}");
+                WriteLine($"Метод: {ex.TargetSite}");
+                WriteLine($"Трассировка стека: {ex.StackTrace}");
+            }
+            ReadKey();
+        }
+    }
+}
