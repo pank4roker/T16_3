15844c52c516a903e1ec8ebdf37d4d5c8980e425
Markow Kirill <122300549+pank4roker@users.noreply.github.com>
2024-01-31 22:09:00 +0200

    Add files via upload
    3 задание

---
+++ 16_3/Proc.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+using static System.Console;
+
+namespace _16_3
+{
+    /// <summary>
+    /// Класс описывающий процедуры
+    /// </summary>
+    class Proc
+    {
+        /// <summary>
+        /// Поле описывающее начало первой процедуры
+        /// </summary>
+        public TimeSpan procedure_1;
+        /// <summary>
+        /// Поле описывающее начало Второй процедуры
+        /// </summary>
+        public TimeSpan procedure_2;
+        /// <summary>
+        /// Общее количество процедур
+        /// </summary>
+        private int countP;
+        /// <summary>
+        /// Constructor without parameters
+        /// </summary>
+        public Proc() { }
+        /// <summary>
+        /// Constructor with parameters
+        /// </summary>
+        /// <param name="procedure_1"></param>
+        /// <param name="procedure_2"></param>
+        /// <param name="countP"></param>
+        public Proc(TimeSpan procedure_1, TimeSpan procedure_2, int countP)
+        {
+            this.procedure_1 = procedure_1;
+            this.procedure_2 = procedure_2;
+            this.countP = countP;
+        }
+        /// <summary>
+        /// Метод вывода 
+        /// </summary>
+        public void OutPut()
+        {
+            WriteLine("Время проведения медицинских процедур:");
+            TimeSpan procedure = procedure_2 - procedure_1;
+            TimeSpan time = procedure_1;
+            for(int i=0;i<countP;i++)
+            {
+                WriteLine($"Процедура №{i + 1}: {time}");
+                time += procedure;
+            }    
+        }
+    }
+}
---
+++ 16_3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace _16_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Write($"Введите время первой процедуры: ");
+            TimeSpan procedure_1 = TimeSpan.Parse(ReadLine());
+            Write($"Введите время второй процедуры: ");
+            TimeSpan procedure_2 = TimeSpan.Parse(ReadLine());
+            Write($"Введите общее количество процедур: ");
+            int countP = Convert.ToInt32(ReadLine());
+
+            Proc procedures = new Proc(procedure_1, procedure_2, countP);
+            procedures.OutPut();
+            ReadKey();
+        }
+    }
+}
